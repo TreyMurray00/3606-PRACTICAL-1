3f9423616a1d3d230bc02dd2b84a0931c60b356d
Trey Murray <77852326+TreyMurray00@users.noreply.github.com>
2022-10-02 09:48:15 -0400

    working

--- app/src/main/java/com/example/p1/MainActivity.java
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -3,35 +3,37 @@ package com.example.p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     
     private TextView total;
     private CheckBox shirt, pants, socks;
-
+    private int sum = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        shirt = (CheckBox) findViewById(R.id.shirt);
+        pants = (CheckBox) findViewById(R.id.pants);
+        socks = (CheckBox) findViewById(R.id.socks);
+        total = (TextView) findViewById(R.id.totalText);
     }
     
-    public void calucateTotal(){
-        private int sum = 0;
-        total = (TextView) findViewById(R.id.totalText);
-        shirt = (CheckBox) findViewById(R.id.checkBox);
-        pants = (CheckBox) findViewById(R.id.checkBox2);
-        socks = (CheckBox) findViewById(R.id.checkBox3);
-        
+    public void calculateTotal(View view) {
+
         if (shirt.isChecked())
             sum += 200;
         if (pants.isChecked())
             sum += 300;
         if (socks.isChecked())
             sum += 50;
-        
-        total.setText("Your total is $" + String.valueOf(sum) + ".00");
+       Toast.makeText(MainActivity.this, "TOTAL: "+ sum, Toast.LENGTH_LONG).show();
+       total.setText("Your total is $" + String.valueOf(sum) + ".00");
+
     }
-        
 }
