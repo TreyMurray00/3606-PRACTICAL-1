e23d24051b77f7ba8ebf0716646671ef2d5e0365
ShivsTKD <77997932+ShivsTKD@users.noreply.github.com>
2022-10-01 22:15:23 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/p1/MainActivity.java
+++ app/src/main/java/com/example/p1/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private TextView total;
+    private CheckBox shirt, pants, socks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void calucateTotal(){
+        private int sum = 0;
+        total = (TextView) findViewById(R.id.totalText);
+        shirt = (CheckBox) findViewById(R.id.checkBox);
+        pants = (CheckBox) findViewById(R.id.checkBox2);
+        socks = (CheckBox) findViewById(R.id.checkBox3);
+        
+        if (shirt.isChecked())
+            sum += 200;
+        else if (pants.isChecked())
+            sum += 300;
+        else if (socks.isChecked())
+            sum += 50;
+        
+        total.setText("Your total is $" + String.valueOf(sum) + ".00");
+    }
+        
 }
